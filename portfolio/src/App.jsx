1bb977ecf27f557d18ca7668acf8297038a3f55b
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-12-16 16:54:55 +0630

    finished project setup and run

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-900 font-semibold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
