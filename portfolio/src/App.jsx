6b90a3df8a1f85542ab18f6be679783b00d4d00c
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-12-31 15:08:11 +0630

    finished tailwind css configurations

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl text-red-900 font-semibold">Hello World</h1>
+      <h1 className="text-4xl text-red-900 font-semibold ">Hello World</h1>
     </div>
   )
 }
