6ec7a14d5c403cebf8c35961e0c81d7774808512
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-01-01 19:44:09 +0630

    finished responsive navbar ui

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useMediaQuery from './hooks/useMediaQuery'
+import Navbar from './scenes/Navbar'
 
 const App = () => {
+  const [selectedPage, setSelectedPage] = useState("home")
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+  const isDesktop = useMediaQuery("(min-width: 1060px)")
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage("home")
+      }
+      if (window.scrollY !== 0) {
+        setIsTopOfPage(false)
+      }
+      window.addEventListener("scroll", handleScroll)
+
+      // cleanup function
+      return () => window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
-    <div>
-      <h1 className="text-4xl text-red-900 font-semibold ">Hello World</h1>
+    <div className='app bg-deep-blue'>
+      <Navbar 
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
